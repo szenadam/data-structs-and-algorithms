1a824b72d7a3814670686d0b40922e975a2012da
szenadam <szenadam1@gmail.com>
2019-04-01 19:30:31 +0200

    Create Struct header (no impl)

--- datastructs.h
+++ datastructs.h
@@ -11,4 +11,45 @@ public:
   E1() { next = NULL; };
 };
 
+class Stack
+{
+private:
+  int n;
+  int A[];
+
+public:
+  Stack(int m);
+  ~Stack();
+  void push(int x);
+  int pop();
+  int top();
+  bool isFull();
+  bool isEmpty();
+  void setEmpty();
+};
+
+Stack::Stack(int m)
+{
+}
+
+void Stack::push(int x)
+{
+}
+
+int Stack::pop()
+{
+}
+int Stack::top()
+{
+}
+bool Stack::isFull()
+{
+}
+bool Stack::isEmpty()
+{
+}
+void Stack::setEmpty()
+{
+}
+
 #endif
